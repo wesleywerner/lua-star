f329ba1d9663175ea0b4955f502b0ba761cdf2a6
Wesley <wesley.werner@gmail.com>
2018-10-27 09:40:14 +0200

    Change license to MIT

--- src/lua-star.lua
+++ src/lua-star.lua
@@ -1,25 +1,17 @@
 --[[
-   lua-star.lua
+    Lua star example - Run with love (https://love2d.org/)
+    Copyright 2018 Wesley Werner <wesley.werner@gmail.com>
 
-   Copyright 2017 wesley werner <wesley.werner@gmail.com>
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   any later version.
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-   You should have received a copy of the GNU General Public License
-   along with this program. If not, see http://www.gnu.org/licenses/.
-
-   References:
-   https://en.wikipedia.org/wiki/A*_search_algorithm
-   https://www.redblobgames.com/pathfinding/a-star/introduction.html
-   https://www.raywenderlich.com/4946/introduction-to-a-pathfinding
+    References:
+    https://en.wikipedia.org/wiki/A*_search_algorithm
+    https://www.redblobgames.com/pathfinding/a-star/introduction.html
+    https://www.raywenderlich.com/4946/introduction-to-a-pathfinding
 ]]--
 
 --- Provides easy A* path finding.
